2616b2c14ce84875d5a978fb83ee60d2df6abb4a
Thomas Calmant <thomas.calmant@gmail.com>
2014-10-06 09:28:21 +0200

    PEP-8 review of jpypex.swing

--- src/python/jpypex/swing/__init__.py
+++ src/python/jpypex/swing/__init__.py
@@ -1,4 +1,4 @@
-#*****************************************************************************
+# *****************************************************************************
 #   Copyright 2004-2008 Steve Menard
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-#*****************************************************************************
+# *****************************************************************************
 
 from .AbstractAction import *
--- src/python/jpypex/swing/pyutils.py
+++ src/python/jpypex/swing/pyutils.py
@@ -1,4 +1,4 @@
-#*****************************************************************************
+# *****************************************************************************
 #   Copyright 2004-2008 Steve Menard
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,29 +13,28 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-#*****************************************************************************
+# *****************************************************************************
 
 from jpype import javax, JObject
 
 __JMenuBar = javax.swing.JMenuBar
 __JMenu = javax.swing.JMenu
 
-def buildMenuBar(menuDef) :
-    mb = __JMenuBar()
 
-    for i in menuDef :
+def buildMenuBar(menuDef):
+    mb = __JMenuBar()
+    for i in menuDef:
         jm = buildMenu(i[0], i[1])
         mb.add(JObject(jm, __JMenu))
-
     return mb
 
-def buildMenu(name, menuDef) :
-    jm = __JMenu(name)
 
-    for i in menuDef :
-        if i is None :
+def buildMenu(name, menuDef):
+    jm = __JMenu(name)
+    for i in menuDef:
+        if i is None:
             jm.addSeparator()
-        elif isinstance(i, list) or isinstance(i, tuple) :
+        elif isinstance(i, list) or isinstance(i, tuple):
             jm2 = buildMenu(i[0], i[1])
             jm.add(jm2)
         else:
